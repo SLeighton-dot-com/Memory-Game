fda0ceae15f5de6be7b3b4e6d35d197621ec4586
SLeighton-dot-com <>
2023-08-08 11:11:41 +0000

    Test.js: Added tests to check the new game function works

--- assets/scripts/game.test.js
+++ assets/scripts/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("./game");
+const { game, newGame, scoreDisplay } = require("./game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -14,18 +14,40 @@ beforeAll(() => {
 
 describe("game object contains correct keys", () => {
     test("score exists", () => {
-        expect("score" in game).toBe(true);
+        expect("score" in game).toEqual(true);
     });
     test("currentGame key exists", () => {
-        expect("currentGame" in game).toBe(true);
+        expect("currentGame" in game).toEqual(true);
     });
     test("playerMoves key exists", () => {
-        expect("playerMoves" in game).toBe(true);
+        expect("playerMoves" in game).toEqual(true);
     });
     test("choices key exists", () => {
-        expect("balls" in game).toBe(true);
+        expect("balls" in game).toEqual(true);
     });
     test("choices contain correct ids", () => {
         expect(game.balls).toEqual(["ball1", "ball2", "ball3", "ball4", "ball5"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["ball1", "ball2"];
+        game.currentGame = ["ball1", "ball2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the computer sequence array", () => {
+        expect(game.currentGame.length).toEqual(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toEqual(0);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
