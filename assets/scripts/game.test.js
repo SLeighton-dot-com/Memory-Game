067eccaa213122a724cd37103d4b2a27e39800b0
SLeighton-dot-com <>
2023-08-08 10:56:29 +0000

    test.js: Added tests to game.test.js file to check for variables

--- assets/scripts/game.test.js
+++ assets/scripts/game.test.js
@@ -2,6 +2,8 @@
  * @jest-environment jsdom
  */
 
+const { game } = require("./game");
+
 beforeAll(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
@@ -11,7 +13,19 @@ beforeAll(() => {
 });
 
 describe("game object contains correct keys", () => {
-    test("score key exists", () => {
+    test("score exists", () => {
         expect("score" in game).toBe(true);
-    })
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("balls" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.balls).toEqual(["ball1", "ball2", "ball3", "ball4", "ball5"]);
+    });
 });
