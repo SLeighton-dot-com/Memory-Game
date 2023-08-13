b195462cecc09b85de8772cb25dbb16219d103c1
SLeighton-dot-com <>
2023-08-13 16:08:36 +0000

    Game.js:Partially coding in movesLeft to count up and down during play

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -6,6 +6,7 @@ let game = {
     numberOfTurns: 0,
     turnInProgress: false,
     lastButton: "",
+    movesLeft: 0,
 };
 
 
@@ -13,6 +14,7 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     game.score = 0;
+    game.movesLeft = 1;
     let balls = document.getElementsByClassName("ball");
     for (let ball of balls) {
         if (!ball.hasAttribute("data-listener")) {
@@ -53,7 +55,10 @@ function showTurns() {
     game.turnInProgress = true;
     game.numberOfTurns = 0;
     let turns = setInterval(() => {
+        console.log("Displaying turn", game.numberOfTurns);
         highlightBall(game.currentGame[game.numberOfTurns]);
+        document.getElementById("left").innerText = game.movesLeft;
+        game.movesLeft++
         game.numberOfTurns++;
         if (game.numberOfTurns >= game.currentGame.length) {
             clearInterval(turns);
@@ -68,10 +73,15 @@ function playerTurn() {
     if (game.currentGame[i] === game.playerMoves[i]) {
         if (game.currentGame.length === game.playerMoves.length) {
             game.score++;
+            game.movesLeft = 1;
             scoreDisplay();
             setTimeout(() => {
                 newTurn();
             }, 1000);
+        } else {
+            game.movesLeft--; // Decrement movesLeft for each correct move by the player
+            console.log("Moves left after correct move:", game.movesLeft);
+            document.getElementById("left").innerText = game.movesLeft; 
         }
     } else {
         alert("We're sorry, that was the wrong ball! Please start a new game.");
