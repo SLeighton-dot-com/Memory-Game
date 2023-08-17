a0440b9205f6dab0d9047a4bdd2f696b70b4fcb8
SLeighton-dot-com <>
2023-08-17 11:50:03 +0000

    Game.js:Added try and catch error handling with console error reporting

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -19,119 +19,203 @@ let game = {
 document.addEventListener("DOMContentLoaded", () => showOverlay());
 
 const showOverlay = () => {
-    document.getElementById('overlay').style.display = 'flex';
+    try {
+        const overlay = document.getElementById('overlay');
+        if (overlay) {
+            overlay.style.display = 'flex';
+        } else {
+            console.error("Element with id 'overlay' not found.");
+        }
+    } catch (error) {
+        console.error("Error in showOverlay:", error);
+    }
 };
 
 const closeOverlay = () => {
-    document.getElementById('overlay').style.display = 'none';
+    try {
+        const overlay = document.getElementById('overlay');
+        if (overlay) {
+            overlay.style.display = 'none';
+        } else {
+            console.error("Element with id 'overlay' not found.");
+        }
+    } catch (error) {
+        console.error("Error in closeOverlay:", error);
+    }
 }
 
 
 
 const newGame = () => {
-    game.currentGame = [];
-    game.playerMoves = [];
-    game.score = 0;
-    game.movesLeft = 0;
-    let balls = document.getElementsByClassName("ball");
-    for (let ball of balls) {
-        if (!ball.hasAttribute("data-listener")) {
-            ball.addEventListener("click", (e) => {
-                if (game.currentGame.length > 0 && !game.turnInProgress) {
-                    let playerMove = e.target.getAttribute("id");
-                    game.lastButton = playerMove;
-                    highlightBall(playerMove);
-                    game.playerMoves.push(playerMove);
-                    game.movesLeft--; 
-                    document.getElementById("left").innerText = game.movesLeft;
-                    playerTurn();
-                }
-            });
-            ball.setAttribute("data-listener", "true");
+    try {
+        game.currentGame = [];
+        game.playerMoves = [];
+        game.score = 0;
+        game.movesLeft = 0;
+
+        let balls = document.getElementsByClassName("ball");
+        for (let ball of balls) {
+            if (!ball.hasAttribute("data-listener")) {
+                ball.addEventListener("click", (e) => {
+                    if (game.currentGame.length > 0 && !game.turnInProgress) {
+                        let playerMove = e.target.getAttribute("id");
+                        game.lastButton = playerMove;
+                        highlightBall(playerMove);
+                        game.playerMoves.push(playerMove);
+                        game.movesLeft--;
+                        const leftElement = document.getElementById("left");
+                        if (leftElement) {
+                            leftElement.innerText = game.movesLeft;
+                        } else {
+                            console.error("Element with id 'left' not found.");
+                        }
+                        playerTurn();
+                    }
+                });
+                ball.setAttribute("data-listener", "true");
+            }
         }
+        scoreDisplay();
+        newTurn();
+    } catch (error) {
+        console.error("Error in newGame:", error);
     }
-    scoreDisplay();
-    newTurn();
 }
 
 const newTurn = () => {
-    game.playerMoves = [];
-    game.currentGame.push(game.balls[(Math.floor(Math.random() * 5))]);
-    game.movesLeft = game.currentGame.length;  
-    showTurns();
-    if (game.timer) {
-        clearInterval(game.timer);
-        game.timer = null;
+    try {
+        game.playerMoves = [];
+        game.currentGame.push(game.balls[(Math.floor(Math.random() * 5))]);
+        game.movesLeft = game.currentGame.length;
+
+        const leftElement = document.getElementById("left");
+        if (leftElement) {
+            leftElement.innerText = game.movesLeft;
+        } else {
+            console.error("Element with id 'left' not found.");
+        }
+        showTurns();
+        if (game.timer) {
+            clearInterval(game.timer);
+            game.timer = null;
+        }
+    } catch (error) {
+        console.error("Error in newTurn:", error);
     }
 }
 
 const scoreDisplay = () => {
-    document.getElementById("score").innerText = game.score;
+    try {
+        const scoreElement = document.getElementById("score");
+        if (scoreElement) {
+            scoreElement.innerText = game.score;
+        } else {
+            console.error("Element with id 'score' not found.");
+        }
+    } catch (error) {
+        console.error("Error in scoreDisplay:", error);
+    }
 }
 
 const highlightBall = (ball) => {
-    let audioElement = document.getElementById("sound_" + ball);
-    if (audioElement) {
-        audioElement.currentTime = 0;
-        audioElement.play();
-    } // Play the sound for the ball
-    document.getElementById(ball).classList.add("highlight");
-    setTimeout(() => {
-        document.getElementById(ball).classList.remove("highlight");
-    }, 300);
+    try {
+        let audioElement = document.getElementById("sound_" + ball);
+        if (audioElement) {
+            audioElement.currentTime = 0;
+            audioElement.play();
+        } else {
+            console.warn(`Audio element for ball ${ball} not found.`);
+        }
+        const ballElement = document.getElementById(ball);
+        if (ballElement) {
+            ballElement.classList.add("highlight");
+            setTimeout(() => {
+                ballElement.classList.remove("highlight");
+            }, 300);
+        } else {
+            console.error(`Element with id ${ball} not found.`);
+        }
+    } catch (error) {
+        console.error("Error in highlightBall:", error);
+    }
 }
 
 const showTurns = () => {
-    game.turnInProgress = true;
-    game.numberOfTurns = 0;
-    let turns = setInterval(() => {
-        document.getElementById("left").innerText = game.movesLeft;
-        highlightBall(game.currentGame[game.numberOfTurns]);
-        game.numberOfTurns++;
-        if (game.numberOfTurns >= game.currentGame.length) {
-            clearInterval(turns);
-            game.turnInProgress = false;
-            startTimer();
+    try {
+        game.turnInProgress = true;
+        game.numberOfTurns = 0;
+
+        const leftElement = document.getElementById("left");
+        if (!leftElement) {
+            throw new Error("Element with id 'left' not found.");
         }
-    }, 800);
+        let turns = setInterval(() => {
+            leftElement.innerText = game.movesLeft;
+            highlightBall(game.currentGame[game.numberOfTurns]);
+            game.numberOfTurns++;
+            if (game.numberOfTurns >= game.currentGame.length) {
+                clearInterval(turns);
+                game.turnInProgress = false;
+                startTimer();
+            }
+        }, 800);
+    } catch (error) {
+        console.error("Error in showTurns:", error);
+    }
 }
 
 
 const playerTurn = () => {
-    let i = game.playerMoves.length - 1;
-    if (game.currentGame[i] === game.playerMoves[i]) {       
-        if (game.currentGame.length === game.playerMoves.length) {
+    try {
+        let i = game.playerMoves.length - 1;
+        if (game.currentGame[i] === game.playerMoves[i]) {
+            if (game.currentGame.length === game.playerMoves.length) {
+                clearInterval(game.timer);
+                game.score++;
+                game.movesLeft = 1;
+                scoreDisplay();
+                setTimeout(() => {
+                    newTurn();
+                }, 1000);
+            }
+        } else {
             clearInterval(game.timer);
-            game.score++;            
-            game.movesLeft = 1;
-            scoreDisplay();
-            setTimeout(() => {
-                newTurn();
-            }, 1000);
+            alert("We're sorry, that was the wrong ball! Please start a new game.");
         }
-    } else {
-        clearInterval(game.timer);
-        alert("We're sorry, that was the wrong ball! Please start a new game.");
+    } catch (error) {
+        console.error("Error in playerTurn:", error);
     }
 }
 
 const startTimer = () => {
-    if (game.timer) return;
-    let timeLeft = game.currentGame.length * 2;
-    updateTimerDisplay(timeLeft);
-    game.timer = setInterval(() => {
-        timeLeft--;
+    try {
+        if (game.timer) return;
+        let timeLeft = game.currentGame.length * 2;
         updateTimerDisplay(timeLeft);
-
-        if (timeLeft <= 0) {
-            clearInterval(game.timer);
-            alert("We're sorry, your time's up! Please start a new game.");
-        }
-    }, 1000);
+        game.timer = setInterval(() => {
+            timeLeft--;
+            updateTimerDisplay(timeLeft);
+            if (timeLeft <= 0) {
+                clearInterval(game.timer);
+                alert("We're sorry, your time's up! Please start a new game.");
+            }
+        }, 1000);
+    } catch (error) {
+        console.error("Error in startTimer:", error);
+    }
 }
 
 const updateTimerDisplay = (seconds) => {
-    document.getElementById("timer").innerText = seconds;
+    try {
+        const timerElement = document.getElementById("timer");
+        if (timerElement) {
+            timerElement.innerText = seconds;
+        } else {
+            console.error("Element with id 'timer' not found.");
+        }
+    } catch (error) {
+        console.error("Error in updateTimerDisplay:", error);
+    }
 }
 
 // module.exports = { game, newGame, scoreDisplay, newTurn };
